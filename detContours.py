574745b8e02db89adb5fdfbe8cf5da7b44794186
DanielCurtis1 <dwcurti1@stu.k12.wv.us>
2022-03-12 16:31:20 -0500

    ouga

--- detContours.py
+++ detContours.py
@@ -4,20 +4,19 @@ import cv2
 import numpy as np
 
 cm = CameraServer()
-#csc = cscore.CvSource("bob", 4)
 
 cm.enableLogging()
 
 camera = cm.startAutomaticCapture()
-camera.setResolution(1600, 900)
-
-#cv2.VideoCapture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc("M", "J", "P", "G"))
+camera.setResolution(1280, 720)
 
 sink = cv2.VideoCapture(0)
 input_img = 0
 
+output = cm.putVideo("Camera rPi", 1280, 720)
+
 # [hue, saturation, value]
-blueMin = np.asarray([0, 0, 0])
+blueMin = np.asarray([50, 50, 50])
 blueMax = np.asarray([360, 360, 360])
 
 redMin = np.asarray([0, 0, 0])
@@ -27,5 +26,6 @@ while True:
     time, input_img = sink.read()
 
     HSV_img = cv2.cvtColor(input_img, cv2.COLOR_RGB2HSV)
-    #csc.putFrame(HSV_img)
     binary_img = cv2.inRange(HSV_img, blueMax, blueMin)
+   
+    output.putFrame(binary_img) 
