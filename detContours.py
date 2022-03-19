90f3dc59170d4288f8ae0aee08b11417ae4d05d0
DanielCurtis1 <dwcurti1@stu.k12.wv.us>
2022-03-19 14:41:27 -0400

    used some help

--- detContours.py
+++ detContours.py
@@ -1,72 +1,86 @@
 from cscore import CameraServer
-from networktables import NetworkTables
+from networktables import NetworkTables as nt
 import time
 import cscore
 import cv2
 import numpy as np
 import json
 
-cm = CameraServer()
 
-cm.enableLogging()
+def main():
+    cm = CameraServer()
 
-with open('/boot/frc.json') as pain:
-      config = json.load(pain)
-camera = config['cameras'][0]
+    cm.enableLogging()
 
-width = camera['width']
-height = camera['height']
+    vision_nt = nt.getTable('Vision')
 
-cm.startAutomaticCapture()
+    with open('/boot/frc.json') as pain:
+        config = json.load(pain)
+    camera = config['cameras'][0]
 
-sink = cv2.VideoCapture(0)
-input_img = 0
+    width = camera['width']
+    height = camera['height']
 
-output = cm.putVideo("E", width, height)
+    cm.startAutomaticCapture()
 
-# [hue, saturation, value]
-blueMin = np.asarray([50, 50, 50])
-blueMax = np.asarray([360, 360, 360])
+    sink = cv2.VideoCapture(0)
+    input_img = 0
 
-redMin = np.asarray([0, 0, 0])
-redMax = np.asarray([25, 360, 360])
+    input_stream = cm.getVideo()
+    output = cm.putVideo("E", width, height)
 
-time.sleep(.05)
+    # [hue, saturation, value]
+    blueMin = np.asarray([0, 0, 0])
+    blueMax = np.asarray([30, 255, 255])
 
-while True:
-    time, input_img = sink.read()
-    output_img = np.copy(input_img)
+    redMin = np.asarray([110, 180, 0])
+    redMax = np.asarray([120, 200, 255])
 
-    #Takes input and converts its to a binary image (to do: make not bad)
-    HSV_img = cv2.cvtColor(input_img, cv2.COLOR_RGB2HSV)
-    binary_img = cv2.inRange(HSV_img, redMin, redMax)
+    isred = True
 
+    time.sleep(.05)
 
-    #removing most of the noise
-    kernel = np.ones((3, 3), np.uint8)
-    binary_img = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel)
+    while True:
+        times, input_img = sink.read()
+        output_img = np.copy(input_img)
 
-    #contour is a pain that I hope no one else has to endure
-    _, contour_list, _ = cv2.findContours(binary_img, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-    x_list = []
-    y_list = []
-
-    for contour in contour_list:
-        if cv2.contourArea(contour) < 15:
+        if times == 0:
+            output.notifyError(input_stream.getError())
             continue
 
-        cv2.drawContours(output_img, contour, -1, color = (255, 255, 255), thickness = -1)
 
-        outline = cv2.minAreaRect(contour)
-        center, size, angle = outline
-        center = tuple([int(dim) for dim in center])
-
-        cv2.drawContours(output_img, [cv2.boxPoints(outline).astype(int)], -1, color = (0, 0, 255), thickness = 2)
-        cv2.circle(output_img, center = center, radius = 3, color = (0, 0, 255), thickness = -1)
-
-        x_list.append((center[0] - width / 2) / (width / 2))
-        x_list.append((center[1] - width / 2) / (width / 2))
+        #Takes input and converts its to a binary image (to do: make not bad (did))
+        HSV_img = cv2.cvtColor(input_img, cv2.COLOR_RGB2HSV)
+        if isred == True:
+            binary_img = cv2.inRange(HSV_img, redMin, redMax)
+        else:
+            binary_img = cv2.inRange(HSV_img, blueMin, blueMax)
 
 
-    print(binary_img)
-    output.putFrame(output_img) 
+        #removing most of the noise
+        kernel = np.ones((3, 3), np.uint8)
+        binary_img = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel)
+
+        #contour is a pain that I hope no one else has to endure
+        _, contour_list, _ = cv2.findContours(binary_img, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contour_list:
+            if cv2.contourArea(contour) < 15:
+                continue
+
+            cv2.drawContours(output_img, contour, -1, color = (255, 255, 255), thickness = -1)
+
+            outline = cv2.minAreaRect(contour)
+            center, size, angle = outline
+            center = tuple([int(dim) for dim in center])
+
+            cv2.drawContours(output_img, [cv2.boxPoints(outline).astype(int)], -1, color = (0, 0, 255), thickness = 2)
+            cv2.circle(output_img, center = center, radius = 3, color = (0, 0, 255), thickness = -1)
+
+        count = 1
+        for element in binary_img:
+            vision_nt.putNumberArray('binaryImgUnwrapped' + str(count), element)
+            count += 1
+
+        print(binary_img)
+main()
